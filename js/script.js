2dc072ad195b5ebe15ef82fc86bd94a1378bb30e
Iliev Artem <aartemiliev@gmail.com>
2025-01-14 16:28:50 +0200

    Upgrade modal screen

--- js/script.js
+++ js/script.js
@@ -105,13 +105,16 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-mclose]');
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     //Можна використовувати toggle(show)
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -133,4 +136,16 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 6000)
+
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.
+            documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll)        
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
